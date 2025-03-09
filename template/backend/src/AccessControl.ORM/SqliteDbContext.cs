586503dec70e4bf44f4a55d1319ab625964e9e7d
Claudio Santana <claudio.a.santana@outlook.com>
2025-03-09 19:17:12 -0300

    Criar contexto para testar o sqlite.

---
+++ template/backend/src/AccessControl.ORM/SqliteDbContext.cs
@@ -0,0 +1,37 @@
+﻿using AccessControl.Domain.Entities;
+using AccessControl.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Be.CDB.Data.Context
+{
+    public class SqliteDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=sqlite_cdb", option =>
+            {
+                option.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .ToTable("Users", "AccessControl")
+                .HasData(new User 
+                {
+                    Username = "mcmoriam",
+                    Email = "mcmoriam@gmail.com",
+                    Password = "mcM0ri@m",
+                    Phone = "21992061136",
+                    Status = UserStatus.Active,
+                    Role = UserRole.Admin
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
