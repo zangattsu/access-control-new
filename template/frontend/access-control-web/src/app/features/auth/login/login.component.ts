58c44aa7b2b9ad2abbee3f074546ab842203a730
mcmoriam <claudio.a.santana@outlook.com>
2025-04-23 18:48:29 -0300

    Criar login.

---
+++ template/frontend/access-control-web/src/app/features/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthStateService } from '@okta/okta-angular';
+
+@Component({
+  selector: 'app-login',
+  template: `<p>Processando login...</p>`,
+  // Removed invalid imports array
+})
+export class LoginComponent implements OnInit {
+  constructor(private oktaAuth: OktaAuthStateService) {}
+
+  ngOnInit(): void {
+    this.oktaAuth.handleLoginRedirect();
+  }
+}
