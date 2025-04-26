d6785f4b0db043a09bc65bd9682dd63ed12fd920
mcmoriam <claudio.a.santana@outlook.com>
2025-04-26 15:21:55 -0300

    login callback

---
+++ template/frontend/access-control-web/src/app/features/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateFn, Router } from "@angular/router";
+import { inject } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { RouterStateSnapshot } from "@angular/router";
+
+export const authGuard: CanActivateFn = async (route, state) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    await authService.checkAuthstatus(); // Ensure the authentication status is checked before proceeding
+
+    if (authService.isAuthenticated()) {
+        return true;
+    }
+
+    router.navigate(["/login"]);
+    return false;
+};
---
+++ template/frontend/access-control-web/src/app/features/auth/login-callback/login-callback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-callback',
+  imports: [],
+  templateUrl: './login-callback.component.html',
+  styleUrl: './login-callback.component.css'
+})
+export class LoginCallbackComponent {
+
+}
