0334ab14048ff9529f101ac0ae50507577953b4a
mcmoriam <claudio.a.santana@outlook.com>
2025-05-19 21:39:06 -0300

    Implementando Dashboard para teste.

---
+++ template/backend/src/AccessControl.WebApi/Features/Dashboard/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using AccessControl.Application.Users.CreateUser;
+using AccessControl.WebApi.Common;
+using AccessControl.WebApi.Features.Users.CreateUser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessControl.WebApi.Features.Dashboard
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashboardController : BaseController
+    {
+        /// <summary>
+        /// Creates a new user
+        /// </summary>
+        /// <param name="request">The user creation request</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created user details</returns>
+        [HttpGet]
+        public IActionResult GetTestAuth()
+        {
+            return Ok(new ApiResponse
+            {
+                Success = true,
+                Message = "Test successfully"
+            });
+        }
+    }
+}
